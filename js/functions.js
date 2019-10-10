43f7e9d9c632685adb07ae47cb309b1f350bdd2c
Brian <brian@megsupporttools.com>
2019-10-10 16:21:42 +0100

    Getting there, needs background image and notification

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,34 @@
 $(document).ready(function(){
-//Handle button clicks and displays
+  // Handle button clicks and displays
+  // sessionStorage.name = $("#name").val();
+  // sessionStorage.story = $("#story").val();
+
+  // Do not change slide unless name field returns a value
+  // $(".next").click(function(){
+  //   if (name == '') {
+  //     console.log(name);
+  //   }
+  //   else {
+  //     handleClick();
+  //   }
+  // });
+
+  // Do not submit unless textarea returns a value
+  // $(".send").click(function(){
+  //   if (story == '') {
+  //     console.log(name);
+  //   }
+  //   else {
+  //     $(".notification").css("display", "block");
+  //   }
+  // });
+
+  // Set initial buttons to be displayed
   $(".next").css("display", "block");
   $(".prev").css("display", "none");
   $(".send").css("display", "none");
 
+  // Set page display
   $(".one").addClass("slideInLeft");
   $(".two").addClass("slideOutRight");
   $(".three").addClass("slideOutRight");
@@ -11,6 +36,7 @@ $(document).ready(function(){
   var page = 0;
   var whichClass = $(".one").hasClass("slideInRight");
 
+  // Handle "Next" click
   $(".next").click(function(){
     switch(page){
       case 0:
@@ -21,17 +47,22 @@ $(document).ready(function(){
             $(".one").addClass("slideOutLeft");
             $(".next").css("display", "block");
             $(".prev").css("display", "block");
+            $(".send").css("display", "none");
             break;
           default:
           $(".one").removeClass("slideInLeft");
           $(".one").addClass("slideOutLeft");
           $(".next").css("display", "block");
           $(".prev").css("display", "block");
+          $(".send").css("display", "none");
         }
         $(".two").removeClass("slideOutRight");
         $(".two").addClass("slideInRight");
         $(".next").css("display", "block");
         $(".prev").css("display", "block");
+        $(".send").css("display", "none");
+        $("#ovalTwo").addClass("active");
+        $("#ovalThree").removeClass("active");
         break;
       case 1:
         page = 2;
@@ -43,12 +74,15 @@ $(document).ready(function(){
         $(".next").css("display", "none");
         $(".prev").css("display", "block");
         $(".send").css("display", "block");
+        $("#ovalTwo").addClass("active");
+        $("#ovalThree").addClass("active");
         break;
       default:
       console.log("Page is: " + page);
     }
   });
 
+  // Handle "Previous" click
   $(".prev").click(function(){
     switch(page){
       case 1:
@@ -59,17 +93,22 @@ $(document).ready(function(){
             $(".one").addClass("slideInLeft");
             $(".next").css("display", "block");
             $(".prev").css("display", "none");
+            $(".send").css("display", "none");
             break;
           default:
           $(".one").removeClass("slideOutLeft");
           $(".one").addClass("slideInLeft");
           $(".next").css("display", "block");
           $(".prev").css("display", "none");
+          $(".send").css("display", "none");
         }
         $(".two").removeClass("slideInRight");
         $(".two").addClass("slideOutRight");
         $(".next").css("display", "block");
         $(".prev").css("display", "none");
+        $(".send").css("display", "none");
+        $("#ovalTwo").removeClass("active");
+        $("#ovalThree").removeClass("active");
         break;
       case 2:
         page = 1;
@@ -79,16 +118,12 @@ $(document).ready(function(){
         $(".two").addClass("slideInLeft");
         $(".next").css("display", "block");
         $(".prev").css("display", "block");
-        $(".send").css("display", "block");
+        $(".send").css("display", "none");
+        $("#ovalTwo").addClass("active");
+        $("#ovalThree").removeClass("active");
         break;
       default:
       console.log("Page is: " + page);
     }
   });
-
-  //Grow text area with content
-  function textAreaAdjust(o){
-    o.style.height = "1px";
-    o.style.height = (25+o.scrollHeight)+"px";
-  }
 });
